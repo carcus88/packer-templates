37d276b2bc82c769ddd241e252111979d8d3b604
Mark Mitchell <mmitchell@riccagroup.com>
2015-09-17 19:50:18 -0400

    Basic setup
    Configured to work with packer v0.8.6 and set up the config for our
    needs

--- scripts/install_hypervisor_tools.ps1
+++ scripts/install_hypervisor_tools.ps1
@@ -9,83 +9,136 @@ function Install-7Zip {
     } else {
         Write-Host "Installing Chocolatey"
         $env:TEMP = 'C:\Windows\Temp'   #chocolatey install will fail without this
-	    iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
-	}
+        iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
+        Write-Host "Enabling Chocolatey global confirmation"
+        choco feature enable -n allowGlobalConfirmation
+
+
+        Write-Host "Checking for Choco-lock-o"
+        Wait-For-File 'C:\ProgramData\chocolatey\chocolateyinstall\install.log'
+        Write-Host "Ok to go on, installing PSCX"
+        choco install pscx
+    }
     Write-Host "Installing 7-Zip"
-	choco install 7zip
+    choco install 7zip
+}
+
+#
+# Wait for a file to not be locked before going on
+#
+function Wait-For-File {
+  $file = $args[0]
+  $timeout = new-timespan -Minutes 1
+  $sw = [diagnostics.stopwatch]::StartNew()
+  while ($sw.elapsed -lt $timeout){
+      if (! (Test-FileLock $file)){
+          return
+      }
+      Write-Host ("File : " + $file + " is locked")
+      start-sleep -seconds 5
+  }
+}
+
+#
+# Test if a file is locked
+#
+function Test-FileLock {
+  param ([parameter(Mandatory=$true)][string]$Path)
+
+  $oFile = New-Object System.IO.FileInfo $Path
+
+  if ((Test-Path -Path $Path) -eq $false)
+  {
+    return $false
+  }
+
+  try
+  {
+      $oStream = $oFile.Open([System.IO.FileMode]::Open, [System.IO.FileAccess]::ReadWrite, [System.IO.FileShare]::None)
+      if ($oStream)
+      {
+        $oStream.Close()
+      }
+      $false
+  }
+  catch
+  {
+    # file is locked by a process.
+    return $true
+  }
 }
 
 function Setup-LocalVBox {
-	# VirtualBox image for local testing (Vagrant)
+    # VirtualBox image for local testing (Vagrant)
 
-	Write-Host "Setting up local VirtualBox"
-	
-	Install-7Zip
-	
-	# There needs to be Oracle CA (Certificate Authority) certificates installed in order
-	#to prevent user intervention popups which will undermine a silent installation.
-	Write-Host "Installing Oracle certificate"
-	cmd /c certutil -addstore -f "TrustedPublisher" A:\oracle-cert.cer
+    Write-Host "Setting up local VirtualBox"
+    
+    Install-7Zip
+    
+    # There needs to be Oracle CA (Certificate Authority) certificates installed in order
+    #to prevent user intervention popups which will undermine a silent installation.
+    Write-Host "Installing Oracle certificate"
+    cmd /c certutil -addstore -f "TrustedPublisher" A:\oracle-cert.cer
 
-	mv "C:\Users\vagrant\VBoxGuestAdditions.iso" C:\Windows\Temp
+    mv "C:\Users\vagrant\VBoxGuestAdditions.iso" C:\Windows\Temp
 
     Write-Host "Decompressing ISO"
-	& 'C:\Program Files\7-Zip\7z.exe' x C:\Windows\Temp\VBoxGuestAdditions.iso -oC:\Windows\Temp\virtualbox
-	Write-Host "Installing VBox Additions"
-	C:\Windows\Temp\virtualbox\VBoxWindowsAdditions.exe /S
-	Start-Sleep -Seconds 30  #make sure install finishes
+    & 'C:\Program Files\7-Zip\7z.exe' x C:\Windows\Temp\VBoxGuestAdditions.iso -oC:\Windows\Temp\virtualbox
+    Write-Host "Installing VBox Additions"
+    C:\Windows\Temp\virtualbox\VBoxWindowsAdditions.exe /S
+    Start-Sleep -Seconds 30  #make sure install finishes
 }
 
 function Setup-LocalParallels {
-	# Parallels image for local testing (Vagrant)
+    # Parallels image for local testing (Vagrant)
 
-	Write-Host "Setting up local Parallels"
+    Write-Host "Setting up local Parallels"
 
-	Install-7Zip
+    Install-7Zip
 
     Write-Host "Decompressing ISO"
-	& 'C:\Program Files\7-Zip\7z.exe' x C:\Users\vagrant\prl-tools.iso -oC:\Users\vagrant\prl-tools
-	Write-Host "Installing Parallels Agent"
-	C:\Users\vagrant\prl-tools\ptagent.exe
-	Start-Sleep -Seconds 30  #make sure install finishes
+    & 'C:\Program Files\7-Zip\7z.exe' x C:\Users\vagrant\prl-tools-win.iso -oC:\Users\vagrant\prl-tools-win
+    Write-Host "Installing Parallels Agent"
+    C:\Users\vagrant\prl-tools-win\ptagent.exe
+    Start-Sleep -Seconds 30  #make sure install finishes
 }
 
 function Setup-ESXi {
-	
-	Write-Host "Setting up ESXi"
+    
+    Write-Host "Setting up ESXi"
 
-	Install-7Zip
+    Install-7Zip
 
-	Write-Host "Downloading VMware Tools"
-	#download VMware installer
-	$progressPreference = 'silentlyContinue'
-	Invoke-WebRequest -OutFile 'C:\Users\vagrant\vmware-tools.iso' $vmware_tools_location
-	
-	#extract the iso and run the installer
-	Write-Host "Decompressing ISO"
-	& 'C:\Program Files\7-Zip\7z.exe' x 'C:\Users\vagrant\vmware-tools.iso' -oC:\Windows\Temp\vmware
-	Write-Host "Installing VMware Tools"
-	C:\Windows\Temp\vmware\setup.exe /S /v "/qn REBOOT=R\"
-	Start-Sleep -Seconds 30  #make sure install finishes
+    Write-Host "Downloading VMware Tools"
+    #download VMware installer
+    $progressPreference = 'silentlyContinue'
+    Invoke-WebRequest -OutFile 'C:\Users\vagrant\vmware-tools.iso' $vmware_tools_location
+    
+    #extract the iso and run the installer
+    Write-Host "Decompressing ISO"
+    & 'C:\Program Files\7-Zip\7z.exe' x 'C:\Users\vagrant\vmware-tools.iso' -oC:\Windows\Temp\vmware
+    Write-Host "Installing VMware Tools"
+    C:\Windows\Temp\vmware\setup.exe /S /v "/qn REBOOT=R\"
+    Start-Sleep -Seconds 30  #make sure install finishes
 }
 
 function Setup-OpenStack {
-	# Even though we're building on VirtualBox, the image will be converted for use with OpenStack
+    # Even though we're building on VirtualBox, the image will be converted for use with OpenStack
 
-	Write-Host "Setting up OpenStack"
+    Write-Host "Setting up OpenStack"
 
-	Write-Host "Downloading cloudbase-init"
-	$progressPreference = 'silentlyContinue'
-	Invoke-WebRequest -OutFile 'C:\Users\vagrant\cloudbaseinit.msi' 'https://www.cloudbase.it/downloads/CloudbaseInitSetup_Beta.msi'
+    Write-Host "Downloading cloudbase-init"
+    $progressPreference = 'silentlyContinue'
+    Invoke-WebRequest -OutFile 'C:\Users\vagrant\cloudbaseinit.msi' 'https://www.cloudbase.it/downloads/CloudbaseInitSetup_Beta.msi'
 
-	Write-Host "Installing cloudbase-init"
-	$p = Start-Process -Wait -PassThru -FilePath msiexec -ArgumentList "/i C:\Users\vagrant\cloudbaseinit.msi /qn /l*v C:\Users\vagrant\cloudbaseinit-log.txt"
-	if ($p.ExitCode -ne 0) {
-	    Write-Host "ERROR: problem installing cloudbase-init!"
-	}
+    Write-Host "Installing cloudbase-init"
+    $p = Start-Process -Wait -PassThru -FilePath msiexec -ArgumentList "/i C:\Users\vagrant\cloudbaseinit.msi /qn /l*v C:\Users\vagrant\cloudbaseinit-log.txt"
+    if ($p.ExitCode -ne 0) {
+        Write-Host "ERROR: problem installing cloudbase-init!"
+    }
 
-	Write-Host "Running SetSetupComplete"
-	& "$ENV:ProgramFiles (x86)\Cloudbase Solutions\Cloudbase-Init\bin\SetSetupComplete.cmd"
+    Write-Host "Running SetSetupComplete"
+    & "$ENV:ProgramFiles (x86)\Cloudbase Solutions\Cloudbase-Init\bin\SetSetupComplete.cmd"
 
     Write-Host "Preparing for sysprep"
     #we have to hack this so that sysprep will run (again?)
@@ -95,7 +148,7 @@ function Setup-OpenStack {
     Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SoftwareProtectionPlatform" -Name SkipRearm -Value 1
 
     Write-Host "Running sysprep"
-	$unattendedXmlPath = "$ENV:ProgramFiles (x86)\Cloudbase Solutions\Cloudbase-Init\conf\Unattend.xml"
+    $unattendedXmlPath = "$ENV:ProgramFiles (x86)\Cloudbase Solutions\Cloudbase-Init\conf\Unattend.xml"
     $p = Start-Process -Wait -PassThru -FilePath "$env:SystemRoot\System32\Sysprep\sysprep.exe" -ArgumentList "/quiet /generalize /oobe /shutdown /unattend:`"$unattendedXmlPath`""
     if ($p.ExitCode -ne 0) {
         Write-Host "ERROR: problem running sysprep!"
@@ -103,33 +156,15 @@ function Setup-OpenStack {
 }
 
 function Install-Features {
-    Install-WindowsFeature NET-Framework-45-ASPNET
     Install-WindowsFeature NET-Framework-45-Core
-    Install-WindowsFeature MSMQ
-    Install-WindowsFeature Web-Server
-    Install-WindowsFeature Web-Common-Http
-    Install-WindowsFeature Web-Performance
-    Install-WindowsFeature Web-Basic-Auth
-    Install-WindowsFeature Web-Digest-Auth
-    Install-WindowsFeature Web-Windows-Auth
-    Install-WindowsFeature Web-Asp-Net45
-    Install-WindowsFeature Web-Mgmt-Service
-    Install-WindowsFeature Web-Scripting-Tools
-}
-
-function Setup-RemoteIIS-Mgmt {
-    Set-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\WebManagement\Server -Name EnableRemoteManagement -Value 1
-    Set-Service -name WMSVC -StartupType Automatic
-    Start-Service WMSVC
 }
 
 Install-Features
-Setup-RemoteIIS-Mgmt
 
 switch ($env:PACKER_BUILD_NAME) {
-	"sb_win2012r2sc_vbox" { Setup-LocalVBox }
-	"sb_win2012r2sc_parallels" { Setup-LocalParallels }
-	default { Write-Host -ForegroundColor Red "Unknown Packer builder!" }
+    "sb_win2012r2sc_vbox" { Setup-LocalVBox }
+    "sb_win2012r2sc_parallels" { Setup-LocalParallels }
+    default { Write-Host -ForegroundColor Red "Unknown Packer builder!" }
 }
 
 
--- scripts/win-updates.ps1
+++ scripts/win-updates.ps1
@@ -1,48 +1,55 @@
 param($global:RestartRequired=0,
         $global:MoreUpdates=0,
-        $global:MaxCycles=5)
+        $global:MaxCycles=5,
+        $MaxUpdatesPerCycle=500)
+
+$Logfile = "C:\Windows\Temp\win-updates.log"
+
+function LogWrite {
+   Param ([string]$logstring)
+   $now = Get-Date -format s
+   Add-Content $Logfile -value "$now $logstring"
+   Write-Host $logstring
+}
 
 function Check-ContinueRestartOrEnd() {
     $RegistryKey = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run"
     $RegistryEntry = "InstallWindowsUpdates"
     switch ($global:RestartRequired) {
-        0 {			
+        0 {
             $prop = (Get-ItemProperty $RegistryKey).$RegistryEntry
             if ($prop) {
-                Write-Host "Restart Registry Entry Exists - Removing It"
+                LogWrite "Restart Registry Entry Exists - Removing It"
                 Remove-ItemProperty -Path $RegistryKey -Name $RegistryEntry -ErrorAction SilentlyContinue
             }
-            
-            Write-Host "No Restart Required"
+
+            LogWrite "No Restart Required"
             Check-WindowsUpdates
-            
+
             if (($global:MoreUpdates -eq 1) -and ($script:Cycles -le $global:MaxCycles)) {
-                #Stop-Service $script:ServiceName -Force
-                #Set-Service -Name $script:ServiceName -StartupType Disabled -Status Stopped
                 Install-WindowsUpdates
             } elseif ($script:Cycles -gt $global:MaxCycles) {
-                Write-Host "Exceeded Cycle Count - Stopping"
-			} else {
-                Write-Host "Done Installing Windows Updates"
-                #Set-Service -Name $script:ServiceName -StartupType Automatic -Status Running
+                LogWrite "Exceeded Cycle Count - Stopping"
+                Invoke-Expression "a:\openssh.ps1 -AutoStart"
+            } else {
+                LogWrite "Done Installing Windows Updates"
+                Invoke-Expression "a:\openssh.ps1 -AutoStart"
             }
         }
         1 {
             $prop = (Get-ItemProperty $RegistryKey).$RegistryEntry
             if (-not $prop) {
-                Write-Host "Restart Registry Entry Does Not Exist - Creating It"
-                Set-ItemProperty -Path $RegistryKey -Name $RegistryEntry -Value "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -File $($script:ScriptPath)"
+                LogWrite "Restart Registry Entry Does Not Exist - Creating It"
+                Set-ItemProperty -Path $RegistryKey -Name $RegistryEntry -Value "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -File $($script:ScriptPath) -MaxUpdatesPerCycle $($MaxUpdatesPerCycle)"
             } else {
-                Write-Host "Restart Registry Entry Exists Already"
+                LogWrite "Restart Registry Entry Exists Already"
             }
-            
-            Write-Host "Restart Required - Restarting..."
-            Stop-Process -ProcessName sshd -Force
-            Restart-Computer -Force
-            exit 0
+
+            LogWrite "Restart Required - Restarting..."
+            Restart-Computer
         }
-        default { 
-            Write-Host "Unsure If A Restart Is Required" 
+        default {
+            LogWrite "Unsure If A Restart Is Required"
             break
         }
     }
@@ -50,109 +57,159 @@ function Check-ContinueRestartOrEnd() {
 
 function Install-WindowsUpdates() {
     $script:Cycles++
-    Write-Host 'Evaluating Available Updates:'
+    LogWrite "Evaluating Available Updates with limit of $($MaxUpdatesPerCycle):"
     $UpdatesToDownload = New-Object -ComObject 'Microsoft.Update.UpdateColl'
-    foreach ($Update in $SearchResult.Updates) {
+    $script:i = 0;
+    $CurrentUpdates = $SearchResult.Updates
+    while($script:i -lt $CurrentUpdates.Count -and $script:CycleUpdateCount -lt $MaxUpdatesPerCycle) {
+        $Update = $CurrentUpdates.Item($script:i)
         if (($Update -ne $null) -and (!$Update.IsDownloaded)) {
             [bool]$addThisUpdate = $false
             if ($Update.InstallationBehavior.CanRequestUserInput) {
-                Write-Host "> Skipping: $($Update.Title) because it requires user input"
+                LogWrite "> Skipping: $($Update.Title) because it requires user input"
             } else {
                 if (!($Update.EulaAccepted)) {
-                    Write-Host "> Note: $($Update.Title) has a license agreement that must be accepted. Accepting the license."
+                    LogWrite "> Note: $($Update.Title) has a license agreement that must be accepted. Accepting the license."
                     $Update.AcceptEula()
                     [bool]$addThisUpdate = $true
+                    $script:CycleUpdateCount++
                 } else {
                     [bool]$addThisUpdate = $true
+                    $script:CycleUpdateCount++
                 }
             }
-        
+
             if ([bool]$addThisUpdate) {
-                Write-Host "Adding: $($Update.Title)"
+                LogWrite "Adding: $($Update.Title)"
                 $UpdatesToDownload.Add($Update) |Out-Null
             }
-		}
+        }
+        $script:i++
     }
-    
+
     if ($UpdatesToDownload.Count -eq 0) {
-        Write-Host "No Updates To Download..."
+        LogWrite "No Updates To Download..."
     } else {
-        Write-Host 'Downloading Updates...'
-        $Downloader = $UpdateSession.CreateUpdateDownloader()
-        $Downloader.Updates = $UpdatesToDownload
-        $Downloader.Download()
+        LogWrite 'Downloading Updates...'
+        $ok = 0;
+        while (! $ok) {
+            try {
+                $Downloader = $UpdateSession.CreateUpdateDownloader()
+                $Downloader.Updates = $UpdatesToDownload
+                $Downloader.Download()
+                $ok = 1;
+            } catch {
+                LogWrite $_.Exception | Format-List -force
+                LogWrite "Error downloading updates. Retrying in 30s."
+                $script:attempts = $script:attempts + 1
+                Start-Sleep -s 30
+            }
+        }
     }
-	
+
     $UpdatesToInstall = New-Object -ComObject 'Microsoft.Update.UpdateColl'
     [bool]$rebootMayBeRequired = $false
-    Write-Host 'The following updates are downloaded and ready to be installed:'
+    LogWrite 'The following updates are downloaded and ready to be installed:'
     foreach ($Update in $SearchResult.Updates) {
         if (($Update.IsDownloaded)) {
-            Write-Host "> $($Update.Title)"
+            LogWrite "> $($Update.Title)"
             $UpdatesToInstall.Add($Update) |Out-Null
-              
+
             if ($Update.InstallationBehavior.RebootBehavior -gt 0){
                 [bool]$rebootMayBeRequired = $true
             }
         }
     }
-    
+
     if ($UpdatesToInstall.Count -eq 0) {
-        Write-Host 'No updates available to install...'
+        LogWrite 'No updates available to install...'
         $global:MoreUpdates=0
         $global:RestartRequired=0
-        #Set-Service -Name $script:ServiceName -StartupType Automatic -Status Running
+        Invoke-Expression "a:\openssh.ps1 -AutoStart"
         break
     }
 
     if ($rebootMayBeRequired) {
-        Write-Host 'These updates may require a reboot'
+        LogWrite 'These updates may require a reboot'
         $global:RestartRequired=1
     }
-	
-    Write-Host 'Installing updates...'
-  
+
+    LogWrite 'Installing updates...'
+
     $Installer = $script:UpdateSession.CreateUpdateInstaller()
     $Installer.Updates = $UpdatesToInstall
     $InstallationResult = $Installer.Install()
-  
-    Write-Host "Installation Result: $($InstallationResult.ResultCode)"
-    Write-Host "Reboot Required: $($InstallationResult.RebootRequired)"
-    Write-Host 'Listing of updates installed and individual installation results:'
+
+    LogWrite "Installation Result: $($InstallationResult.ResultCode)"
+    LogWrite "Reboot Required: $($InstallationResult.RebootRequired)"
+    LogWrite 'Listing of updates installed and individual installation results:'
     if ($InstallationResult.RebootRequired) {
         $global:RestartRequired=1
     } else {
         $global:RestartRequired=0
     }
-    
+
     for($i=0; $i -lt $UpdatesToInstall.Count; $i++) {
         New-Object -TypeName PSObject -Property @{
             Title = $UpdatesToInstall.Item($i).Title
             Result = $InstallationResult.GetUpdateResult($i).ResultCode
         }
+        LogWrite "Item: " $UpdatesToInstall.Item($i).Title
+        LogWrite "Result: " $InstallationResult.GetUpdateResult($i).ResultCode;
     }
-	
+
     Check-ContinueRestartOrEnd
 }
 
 function Check-WindowsUpdates() {
-    Write-Host "Checking For Windows Updates"
+    LogWrite "Checking For Windows Updates"
     $Username = $env:USERDOMAIN + "\" + $env:USERNAME
- 
+
     New-EventLog -Source $ScriptName -LogName 'Windows Powershell' -ErrorAction SilentlyContinue
- 
+
     $Message = "Script: " + $ScriptPath + "`nScript User: " + $Username + "`nStarted: " + (Get-Date).toString()
 
     Write-EventLog -LogName 'Windows Powershell' -Source $ScriptName -EventID "104" -EntryType "Information" -Message $Message
-    Write-Host $Message
+    LogWrite $Message
 
     $script:UpdateSearcher = $script:UpdateSession.CreateUpdateSearcher()
-    $script:SearchResult = $script:UpdateSearcher.Search("IsInstalled=0 and Type='Software' and IsHidden=0")      
+    $script:successful = $FALSE
+    $script:attempts = 0
+    $script:maxAttempts = 12
+    while(-not $script:successful -and $script:attempts -lt $script:maxAttempts) {
+        try {
+            $script:SearchResult = $script:UpdateSearcher.Search("IsInstalled=0 and Type='Software' and IsHidden=0")
+            $script:successful = $TRUE
+        } catch {
+            LogWrite $_.Exception | Format-List -force
+            LogWrite "Search call to UpdateSearcher was unsuccessful. Retrying in 10s."
+            $script:attempts = $script:attempts + 1
+            Start-Sleep -s 10
+        }
+    }
+
     if ($SearchResult.Updates.Count -ne 0) {
-        $script:SearchResult.Updates |Select-Object -Property Title, Description, SupportUrl, UninstallationNotes, RebootRequired, EulaAccepted |Format-List
-        $global:MoreUpdates=1
+        $Message = "There are " + $SearchResult.Updates.Count + " more updates."
+        LogWrite $Message
+        try {
+            for($i=0; $i -lt $script:SearchResult.Updates.Count; $i++) {
+              LogWrite $script:SearchResult.Updates.Item($i).Title
+              LogWrite $script:SearchResult.Updates.Item($i).Description
+              LogWrite $script:SearchResult.Updates.Item($i).RebootRequired
+              LogWrite $script:SearchResult.Updates.Item($i).EulaAccepted
+          }
+            $global:MoreUpdates=1
+        } catch {
+            LogWrite $_.Exception | Format-List -force
+            LogWrite "Showing SearchResult was unsuccessful. Rebooting."
+            $global:RestartRequired=1
+            $global:MoreUpdates=0
+            Check-ContinueRestartOrEnd
+            LogWrite "Show never happen to see this text!"
+            Restart-Computer
+        }
     } else {
-        Write-Host 'There are no applicable updates'
+        LogWrite 'There are no applicable updates'
         $global:RestartRequired=0
         $global:MoreUpdates=0
     }
@@ -165,20 +222,11 @@ $script:UpdateSession.ClientApplicationID = 'Packer Windows Update Installer'
 $script:UpdateSearcher = $script:UpdateSession.CreateUpdateSearcher()
 $script:SearchResult = New-Object -ComObject 'Microsoft.Update.UpdateColl'
 $script:Cycles = 0
+$script:CycleUpdateCount = 0
 
-#$script:ServiceName = "OpenSSHd"
-
-#Stop-Service $script:ServiceName -Force
-#Set-Service -Name $script:ServiceName -StartupType Disabled -Status Stopped
-
-if (!(Test-Path 'C:\packer-updates.txt')) {
-
-    Check-WindowsUpdates
-    if ($global:MoreUpdates -eq 1) {
-        Install-WindowsUpdates
-    } else {
-        Check-ContinueRestartOrEnd
-    }
+Check-WindowsUpdates
+if ($global:MoreUpdates -eq 1) {
+    Install-WindowsUpdates
 } else {
-    Write-Host "Updates already completed"
+    Check-ContinueRestartOrEnd
 }
